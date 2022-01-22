5e650f3be76979e03448b9c7e24e160369d624e8
Lucas William <lucaswilliameufrasio@gmail.com>
2022-01-22 00:56:54 -0300

    style: lint

--- resize.ts
+++ resize.ts
@@ -4,41 +4,49 @@ import exifr from 'exifr'
 import fs, { createWriteStream } from 'fs'
 
 async function resizeImages() {
-    const folderPath = path.resolve(__dirname, '..', 'images')
-    const resizedFolder = path.resolve(folderPath, 'resized')
-    if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath)
-    }
-    if (!fs.existsSync(resizedFolder)) {
-        fs.mkdirSync(resizedFolder)
-    }
-    const files = fs.readdirSync(folderPath)
+  const folderPath = path.resolve(__dirname, '..', 'images')
+  const resizedFolder = path.resolve(folderPath, 'resized')
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath)
+  }
+  if (!fs.existsSync(resizedFolder)) {
+    fs.mkdirSync(resizedFolder)
+  }
+  const files = fs.readdirSync(folderPath)
 
-    console.log(files)
+  console.log(files)
 
-    await Promise.all(files.map(async (file) => {
-        if (file === 'resized') {
-            return
-        }
-        sharp.cache(false)
-        const [, extension] = file.split('.')
-        const filePath = `${folderPath}/${file}`
+  await Promise.all(
+    files.map(async (file) => {
+      if (file === 'resized') {
+        return
+      }
+      sharp.cache(false)
+      const [, extension] = file.split('.')
+      const filePath = `${folderPath}/${file}`
 
-        const imageMetadata = await exifr.parse(filePath)
-        const imageWidth = imageMetadata.ImageWidth
-        const imageHeight = imageMetadata.ImageHeight
-        console.log(imageMetadata)
+      const imageMetadata = await exifr.parse(filePath)
+      const imageWidth = imageMetadata.ImageWidth
+      const imageHeight = imageMetadata.ImageHeight
+      console.log(imageMetadata)
 
-        const writeStream = createWriteStream(path.resolve(resizedFolder, file))
+      const writeStream = createWriteStream(path.resolve(resizedFolder, file))
 
-        if (extension === 'jpeg' || extension === 'jpg') {
-            sharp(filePath).resize(imageWidth, imageHeight).jpeg({ quality: 70 }).pipe(writeStream)
-        } else {
-            sharp(filePath).resize(imageWidth, imageHeight).png({ quality: 70 }).pipe(writeStream)
-        }
-}))
+      if (extension === 'jpeg' || extension === 'jpg') {
+        sharp(filePath)
+          .resize(imageWidth, imageHeight)
+          .jpeg({ quality: 70 })
+          .pipe(writeStream)
+      } else {
+        sharp(filePath)
+          .resize(imageWidth, imageHeight)
+          .png({ quality: 70 })
+          .pipe(writeStream)
+      }
+    }),
+  )
 }
 
-(async function () {
-    await resizeImages()
+;(async function () {
+  await resizeImages()
 })()
